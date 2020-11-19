cd62a431c30a536ae9aa65496aa034f75322f97f
Steven <steven@ceriously.com>
2020-11-19 18:12:03 -0500

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,4 @@
 const { PrismaClient } = require('../../prisma/generated/client')
-const fs = require('fs')
-const path = require('path')
 
 export default async (req, res) => {
   const prisma = new PrismaClient()
@@ -8,8 +6,6 @@ export default async (req, res) => {
     const users = await prisma.user.findMany()
     res.status(200).json(users)
   } catch (e) {
-    path.join(process.cwd(), 'prisma/generated/client/index.js')
-
     res.status(200).send(e.stack)
   }
 }
